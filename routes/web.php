d63d57aee65929f84ace7e0311ded52187ff73b0
Rezam21 <reza.marchyana@gmail.com>
2022-10-21 14:16:58 +0700

    video 3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Reza marchyana",
+        "email" => "203040079@unpas.mail.ac.id",
+        "image" => "contoh.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
